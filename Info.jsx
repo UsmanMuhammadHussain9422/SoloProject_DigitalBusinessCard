8b0b779d90261ca3ad70f79359a07d073ba93a2e
UsmanMuhammadHussain9422 <usmanmuhammad9422@gmail.com>
2024-07-17 20:40:32 +0200

    Changed the way images/icons are loaded in the Info.jsx file.

--- Info.jsx
+++ Info.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
-
+import Profile_Photo from './Images/Image_1.png'
+import Email_Icon from './Icons/Email_Icon.png';
+import LinkedIn_Icon from './Icons/LinkedIn_Icon.png';
 
 export default function Info() {
   return (
     <div className='info--container'>
-        <img src="/Images/Image_1.png" />
-        <p className='name'>Laura Smith</p>
-        <p className='role'>Frontend Developer</p>
-        <a className='website'>laurasmith.website</a>
-        <div className='button--container'>
-          <button className='email'>
-            Email
-            <img src='/Icons/Email_Icon.png'/>
-          </button>
-          <button className='linkedin'>
-            LinkedIn
-            <img src='/Icons/LinkedIn_Icon.png'/>
-          </button>
-        </div>
+      <img src={Profile_Photo} />
+      <p className='name'>Laura Smith</p>
+      <p className='role'>Frontend Developer</p>
+      <a className='website'>laurasmith.website</a>
+      <div className='button--container'>
+        <button className='email'>
+          Email
+          <img src={Email_Icon} />
+        </button>
+        <button className='linkedin'>
+          LinkedIn
+          <img src={LinkedIn_Icon} />
+        </button>
+      </div>
     </div>
   )
 }
